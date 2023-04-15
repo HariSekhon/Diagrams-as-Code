5d6557f431567500e2b0ab41afcd9cfb95c47065
Hari Sekhon <harisekhon@gmail.com>
2023-04-15 20:49:23 +0100

    added web_services.py

---
+++ web_services.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#  coding=utf-8
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-04-14 13:54:52 +0100 (Fri, 14 Apr 2023)
+#
+#  https://github.com/HariSekhon/Diagrams-as-Code
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+"""
+
+Web Services
+
+"""
+
+__author__ = 'Hari Sekhon'
+__version__ = '0.1'
+
+from diagrams import Diagram, Cluster
+
+# AWS resources:
+#
+#   https://diagrams.mingrammer.com/docs/nodes/aws
+#
+from diagrams.aws.compute import ECS
+from diagrams.aws.database import RDS, ElastiCache
+from diagrams.aws.network import ELB, Route53
+
+with Diagram("Clustered Web Services", show=True):
+    dns = Route53("Route53")
+    lb = ELB("ELB")
+
+    with Cluster("Web Services"):
+        svc_group = [ECS("web2"),  # middle
+                     ECS("web3"),  # puts below
+                     ECS("web1")]  # puts above
+                     # this is why it's ordered weirdly here but comes out ordered in diagram
+
+    with Cluster("RDS Cluster"):
+        # pylint: disable=W0106
+        db_primary = RDS("userdb")
+        db_primary - [RDS("userdb ro")]
+
+    memcached = ElastiCache("ElastiCache\nmemcached")
+
+    # pylint: disable=W0104
+    dns >> lb >> svc_group
+    svc_group >> db_primary
+    svc_group >> memcached
