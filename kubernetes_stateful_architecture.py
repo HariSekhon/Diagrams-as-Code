47ffc2b0fa621fa3753527e2647c99ddd92881fd
Hari Sekhon <harisekhon@gmail.com>
2023-04-16 17:08:34 +0100

    updated kubernetes_stateful_architecture.py

--- kubernetes_stateful_architecture.py
+++ kubernetes_stateful_architecture.py
@@ -35,7 +35,7 @@ from diagrams.k8s.compute import Pod, StatefulSet
 # from diagrams.k8s.network import Service
 from diagrams.k8s.storage import PV, PVC, StorageClass
 
-with Diagram("Kubernetes Stateful Architecture", show=bool(os.environ.get('CI', 0))):
+with Diagram("Kubernetes Stateful Architecture", show=not bool(os.environ.get('CI', 0))):
     with Cluster("App"):
         # Service clutters this diagram
         # svc = Service("Service)")
