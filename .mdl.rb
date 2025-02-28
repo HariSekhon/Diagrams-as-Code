e9092e8e6a7d198be95d88fe623a28180c2d2f39
Hari Sekhon <harisekhon@gmail.com>
2025-02-28 13:54:56 +0700

    updated .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -16,7 +16,7 @@
 all
 #exclude_rule 'MD001'
 #exclude_rule 'MD003'
-#exclude_rule 'MD005'
+exclude_rule 'MD005'  # https://github.com/markdownlint/markdownlint/issues/296
 exclude_rule 'MD007'  # leave 2 space indentation for lists, 3 space is ugly af
 #exclude_rule 'MD012'
 exclude_rule 'MD013'  # long lines cannot be split if they are URLs
